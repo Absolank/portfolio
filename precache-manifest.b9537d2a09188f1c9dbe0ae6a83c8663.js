28bf59da51c1611b643fd4c27d38a3df4d672e8c
Absolank <ankitkripal@gmail.com>
2020-07-24 23:48:51 +0530

    Updates

---
+++ precache-manifest.b9537d2a09188f1c9dbe0ae6a83c8663.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bca43e1067cb4f52e7777080862d479",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28967216f96349f027c6",
+    "url": "/static/css/2.d81f0aac.chunk.css"
+  },
+  {
+    "revision": "3cd78ef8b005b984e2bd",
+    "url": "/static/css/main.84219b62.chunk.css"
+  },
+  {
+    "revision": "28967216f96349f027c6",
+    "url": "/static/js/2.5f83d5ac.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.5f83d5ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cd78ef8b005b984e2bd",
+    "url": "/static/js/main.8cab5bcf.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  }
+]);
